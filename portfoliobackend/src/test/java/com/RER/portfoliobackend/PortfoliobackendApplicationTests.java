1bed99aa9d49bb84afa56543e389aa884e629cc2
roger349 <roger349@gmail.com>
2022-11-03 02:31:29 -0300

    agregado archivos java spring boot

---
+++ portfoliobackend/src/test/java/com/RER/portfoliobackend/PortfoliobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RER.portfoliobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfoliobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
